3894c8c1c18fe64239c1c3d6a2767094f4d8e65e
George Ruellan <ruellan.george@gmail.com>
2020-12-22 10:08:24 +0000

    Single layer of neurons

---
+++ neural-networks/single-layer.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+'''
+Single neuron layer
+'''
+
+inputs = [1.0, 2.0, 3.0, 2.5]
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+biases = [2, 3, 0.5]
+
+# y = mx + b
+output = np.dot(weights, inputs) + biases
+print(output)
