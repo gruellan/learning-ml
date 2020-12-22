1dbb125d2cc33ee1a17640b3870f815bf36faae6
George Ruellan <ruellan.george@gmail.com>
2020-12-22 10:31:26 +0000

    Matrix product

--- neural-networks/single-layer.py
+++ neural-networks/single-layer.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 '''
-Single neuron layer
+Single neuron layer with one data point
 '''
 
 inputs = [1.0, 2.0, 3.0, 2.5]
@@ -10,6 +10,26 @@ weights = [[0.2, 0.8, -0.5, 1.0],
            [-0.26, -0.27, 0.17, 0.87]]
 biases = [2, 3, 0.5]
 
+# Dot product
 # y = mx + b
 output = np.dot(weights, inputs) + biases
 print(output)
+
+'''
+Single neuron layer with batch of data points
+'''
+inputs = [[1.0, 2.0, 3.0, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+biases = [2.0, 3.0, 0.5]
+
+# Matrix product takes all of the combos of rows from the left matrix and
+# columns from the right matrix, performing the dot product on them.
+# Requires num cols of first matrix == num rows of second matrix
+# but currently ours have the same shape (3, 4) so we transpose weights.
+# Now we have inputs = (3, 4) and weights = (4, 3) so we can perform matrix product
+layer_outputs = np.dot(inputs, np.array(weights).T) + biases
+print(layer_outputs)
